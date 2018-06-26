57e859a006538a00f1601ef5014474e1d0e50643
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-06-26 23:12:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,7 +41,7 @@ def turn(board)
   index = gets.strip
   input_to_index(index)
   
-if valid_move(board, index) == TRUE
+if valid_move?(board, index) == TRUE
   move(board, index, value = "X")
   display_board(board)
 else
