627527afc328ba83a4d08531e2f139a2e64277c0
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-06-26 23:14:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,7 +22,7 @@ end
 
 def valid_move?(board, index)
   
-  if index.between(0,8) && position_taken?(board, index) == FALSE
+  if index.between?(0,8) && position_taken?(board, index) == FALSE
    TRUE
  else
    FALSE
